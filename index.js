a1b3e4d826a26593be23aa92d476ebff0f693008
Rin Ng <Rin08.ng@gmail.com>
2018-05-10 19:39:51 +0700

    Added code to callback

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+function match_data(parent_file, children_file, cb) {
+  fs.readFile(parent_file, "utf8", (err, data)=> {
+    if(err){
+      throw err;
+    }
+    sleep.sleep(5);
+    let parents_data = JSON.parse(data);
+
+    fs.readFile(children_file, "utf8", (err,data) => {
+      if(err) throw err;
+      let childrens_data = JSON.parse(data);
+
+      cb(parents_data, childrens_data)
+    });
+  });
 }
 
-match_data('./parents.json', './childrens.json')
+function addChildren(parents_data, childrens_data){
+  for(let i = 0; i < parents_data.length; i++){
+    parents_data[i].children = [];
+    for(let k = 0; k < childrens_data.length; k++){
+      if(parents_data[i].last_name === childrens_data[k].family){
+        sleep.sleep(5);
+        parents_data[i].children.push(childrens_data[k].full_name);
+      }
+    }
+  }
+  console.log(parents_data);
+}
+
+match_data('./parents.json', './childrens.json', addChildren)
 console.log("Notification : Data sedang diproses !");
