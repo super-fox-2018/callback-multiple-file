f3cc2dd168ebdf618a440654e2cb404ac30b8384
mario <panggihmario@gmail.com>
2018-05-10 21:33:04 +0700

    multiple

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
+let parentData = fs.readFileSync('parents.json','utf8')
 
-function match_data(parent_file, children_file) {
-  // your code here...
+
+
+function match_data(parent_file, children_file,call) {
+  fs.readFile(parent_file,'utf8',(err,dataParent)=>{
+    let parentList = JSON.parse(parentData)
+    fs.readFile(children_file,'utf8',(err,dataChildren)=>{
+      let childList =JSON.parse(dataChildren)
+     
+      let arr =[]
+      for(let i=0;i<parentList.length;i++){
+        parentList[i].childrens = []
+      }
+     
+      for(let i=0;i<parentList.length;i++){
+        for(let j =0;j<childList.length;j++){
+   
+          if(parentList[i].last_name == childList[j].family){
+                parentList[i].childrens.push(childList[j].full_name)
+          }
+        }
+      }
+      call(parentList)
+    })
+  })
+  
+
 }
 
-match_data('./parents.json', './childrens.json')
+match_data('./parents.json', './childrens.json',function(call){
+  console.log(call)
+})
 console.log("Notification : Data sedang diproses !");
+
