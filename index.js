a358faf2201b8753be628e404f59ff7feb37a59d
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-11 01:21:42 +0700

    update tanpa sleep.sleep(5)

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+function match_data(parent_file, children_file, cb) {
+
+  fs.readFile(parent_file, (err, dataParent) => {
+    if (err == true) {
+      throw err;
+    }
+
+    let parseParent = JSON.parse(dataParent)
+
+    fs.readFile(children_file, (err, data) => {
+      if (err  == true) {
+        throw err;
+      }
+      let parseChild = JSON.parse(data);
+      // sleep.sleep(5) /* error kalo pake sleep. padahal sudah di update npm nya*/
+      cb(parseParent, parseChild);
+    });
+  });
 }
 
-match_data('./parents.json', './childrens.json')
+function mixedData(parents, children) {
+
+  for (let i = 0; i < parents.length; i++) {
+    let childrenName = [];
+    let check = false;
+    for (let j = 0; j < children.length; j++) {
+      if (parents[i].last_name === children[j].family) {
+        let check = true;
+        if (childrenName.includes(children[j].full_name) === false) {
+          childrenName.push(children[j].full_name);
+        }
+      }
+    }
+
+    if (check == false) {
+      parents[i].childrens = childrenName;
+      console.log(parents[i]);
+    }
+
+  }
+}
+
+match_data('./parents.json', './childrens.json', mixedData);
 console.log("Notification : Data sedang diproses !");
